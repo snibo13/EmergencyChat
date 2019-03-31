9a8cb0eab571c05a5f6e526a11383acd1a8d91e3
Sidney NiBo <snibo13@gmail.com>
2019-03-30 21:24:23 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static(__dirname));
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(usr, msg) {
+    console.log(msg);
+    io.emit('chat message', usr, msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
